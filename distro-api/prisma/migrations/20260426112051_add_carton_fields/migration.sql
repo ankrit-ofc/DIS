580c145ba0b51017a1064979ee0a5411f7492f0a
ankrit-ofc <ankrit.ofc@gmail.com>
2026-04-27 22:05:09 +0545

    fix(prisma): correct table name casing for Linux

--- distro-api/prisma/migrations/20260426112051_add_carton_fields/migration.sql
+++ distro-api/prisma/migrations/20260426112051_add_carton_fields/migration.sql
@@ -1,9 +1,9 @@
 -- AlterTable
-ALTER TABLE `order` ADD COLUMN `vat` DOUBLE NOT NULL DEFAULT 0;
+ALTER TABLE `Order` ADD COLUMN `vat` DOUBLE NOT NULL DEFAULT 0;
 
 -- AlterTable
-ALTER TABLE `orderitem` ADD COLUMN `piecesPerCarton` INTEGER NULL;
+ALTER TABLE `OrderItem` ADD COLUMN `piecesPerCarton` INTEGER NULL;
 
 -- AlterTable
-ALTER TABLE `product` ADD COLUMN `piecesPerCarton` INTEGER NULL,
+ALTER TABLE `Product` ADD COLUMN `piecesPerCarton` INTEGER NULL,
     ADD COLUMN `pricePerCarton` DECIMAL(10, 2) NULL;
